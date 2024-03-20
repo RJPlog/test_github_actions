8f06194362daba9ad07bdf85bfc065cc455b53bf
RJPlog <tfrenz67@gmail.com>
2024-03-20 17:54:56 +0100

    Update hello.kt

--- hello.kt
+++ hello.kt
@@ -1,4 +1,5 @@
 fun main() {
     println("Hello, World! Build worked, let's see if it is running :-) or not.")
     println("Yeah, finally it is running")
+    prinltn("and what about if a bug is there?")
 }
