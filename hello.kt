2e3235a602ec08189edd48e7a7850b0aacd98fc5
RJPlog <tfrenz67@gmail.com>
2024-03-20 22:02:38 +0100

    Update hello.kt

--- hello.kt
+++ hello.kt
@@ -1,4 +1,5 @@
 fun main() {
+    // try to get an abort of workflow at warning with compiler option -Werror
     var x = 0
     println("Hello, World! Build worked, let's see if it is running :-) or not.")
     println("Yeah, finally it is running")
