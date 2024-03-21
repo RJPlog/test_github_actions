1611bc8be48a42b2e73007ede1b4b834e8462ee4
RJPlog <tfrenz67@gmail.com>
2024-03-21 14:37:25 +0100

    Update hello.kt

--- hello.kt
+++ hello.kt
@@ -3,8 +3,7 @@ fun main(args: Array<String>) {
     if (!args.isEmpty()) {
         x = args[0]
     }
-        
-    var x = args[0]
+    x = args[0]
     println("Hello, World! Build worked, let's see if it is running :-) or not.")
     println("Yeah, finally it is running")
     println("and what about if a bug is there?")
