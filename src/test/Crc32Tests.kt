3e85649efc9eed5232100f72c4700a67258566eb
RJPlog <tfrenz67@gmail.com>
2024-03-26 15:10:03 +0100

    Create Crc32Tests.kt

---
+++ src/test/Crc32Tests.kt
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+import java.util.zip.CRC32
+
+class Crc32Tests {
+    fun crc32(input: String): Long {
+        val crc32 = CRC32()
+        crc32.update(input.toByteArray())
+        return crc32.value
+    }
+
+    @Test
+    fun testEmptyString() {
+        val crc = crc32("")
+        assertEquals(0L, crc)
+    }
+
+    @Test
+    fun testSingleCharacter() {
+        val crc = crc32("A")
+        assertEquals(65L, crc)
+    }
+
+    @Test
+    fun testMultipleCharacters() {
+        val crc = crc32("Hello, World!")
+        assertEquals(1243066710L, crc)
+    }
+}
