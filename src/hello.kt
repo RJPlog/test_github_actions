beb7f89b951d4bbffb10a71c7f9e04efeb709440
RJPlog <tfrenz67@gmail.com>
2024-03-26 15:01:10 +0100

    Create hello.kt

---
+++ src/hello.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    var x: String = "42"
+    if (!args.isEmpty()) {
+        x = args[0]
+    }
+    println("Hello, World! Build worked, let's see if it is running :-) or not.")
+    println("Yeah, finally it is running")
+    println("and what about if a bug is there?")
+    println("+++++++++++")
+    println("Eingabe: $x")
+    println("+++++++++++")
+}
