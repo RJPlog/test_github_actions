f21e1e5df7a38954470167720811d5a7601847a7
RJPlog <tfrenz67@gmail.com>
2024-03-27 16:57:53 +0100

    Update crc32.kt

--- src/crc32.kt
+++ src/crc32.kt
@@ -1,5 +1,14 @@
 import java.util.zip.CRC32
 
+// füge eine funktoin hinzu, die eine MD5 hash erzeugt
+fun md5(input: String): String {
+    val md = java.security.MessageDigest.getInstance("MD5")
+    val byteArray = md.digest(input.toByteArray())
+    return byteArray.joinToString("") {
+        "%02x".format(it)
+    }
+}
+
 // schreibe eine  Funktion die eine CRC32 berechnet
 fun crc32(input: String): Long {
     val crc32 = CRC32()
@@ -10,6 +19,7 @@ fun crc32(input: String): Long {
 fun main() {
     for (i in 'A'..'Z') {
         val crc = crc32(i.toString())
-        println("CRC32 of $i is $crc")
+        val hash = md5(i.toString())
+        println("CRC32 of $i is $crc, MD5 Hash ist: $hash")
     }
 }
