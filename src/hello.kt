28c9b8800c82b2ff958a76ed04f0524bd3c3b782
RJPlog <tfrenz67@gmail.com>
2024-10-28 17:24:40 +0100

    Update hello.kt

--- src/hello.kt
+++ src/hello.kt
@@ -1,7 +1,11 @@
 fun main(args: Array<String>) {
     var x: String = "42"
+    var y: String = "empty"
     if (!args.isEmpty()) {
-        x = args[1]
+        x = args[0]
+        if (args.size() > 1) {
+            y = args[1]
+            
     }
     println("Hello, World! Build worked, let's see if it is running :-) or not.")
     println("Yeah, finally it is running")
@@ -10,6 +14,7 @@ fun main(args: Array<String>) {
     println("and it is connected to a branch rule")
     println("+++++++++++")
     println("Eingabe: $x")
+    println("y: $y")
     println("+++++++++++")
     println("ups, bug fixed")
 }
