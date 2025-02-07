75405010f718b79399c54b9249c67cf823d6bca1
RJPlog <tfrenz67@gmail.com>
2025-02-07 13:34:38 +0100

    Create day2224_1_2.kt

---
+++ src/day2224_1_2.kt
@@ -0,0 +1,23 @@
+// sudo apt-get update && sudo apt-get install kotlin
+// kotlinc day2224_1_2.kt -include-runtime -d day2224_1_2.jar && java -jar -Xss515m day2224_1_2.jar
+
+import java.io.File
+import kotlin.math.*
+
+fun 2224(): Int {
+
+  return -1
+}
+
+fun main() {
+
+    var t1 = System.currentTimeMillis()
+
+    println("--- Day 24: Blizzard Basin ---")
+
+    var solution1 = aocDay2224()
+    println("   the fewest number of minutes required to avoid the blizzards and reach the goal is $solution2")
+
+    t1 = System.currentTimeMillis() - t1
+    println("puzzle solved in ${t1} ms")
+}  
