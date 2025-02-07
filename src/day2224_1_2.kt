e41e70c5f282d7b51edfc4f2b9ed5cfddcdc5dff
RJPlog <tfrenz67@gmail.com>
2025-02-07 13:49:12 +0100

    Update day2224_1_2.kt

--- src/day2224_1_2.kt
+++ src/day2224_1_2.kt
@@ -4,8 +4,38 @@
 import java.io.File
 import kotlin.math.*
 
-fun 2224(): Int {
+fun aocDay2224(): Int {
 
+  var w = -2
+  var h = -2
+  var x = -1
+  var y = -1
+
+  // store each wind in list (westWind, ....)
+  var wW = mutableListOf<Pair<Int,Int>>()
+  var nW = mutableListOf<Pair<Int,Int>>()
+  var eW = mutableListOf<Pair<Int,Int>>()
+  var sW = mutableListOf<Pair<Int,Int>>()
+
+  File("day2224_puzzle_input.txt").forEachLine {
+    w = it.length
+    x = -1
+    it.forEach {
+      when(it) {
+        '<' -> wW.add(Pair(x,y))
+        '>' -> eW.add(Pair(x,y))
+        'v' -> nW.add(Pair(x,y))
+        '^' -> sW.add(Pair(x,y))
+      }
+      x += 1
+    }
+    y += 1
+  }
+  
+  // start moving in all posible directions (until first path reached end)
+  for (i in 0..5) {// replace by while later
+    
+  }
   return -1
 }
 
@@ -16,7 +46,7 @@ fun main() {
     println("--- Day 24: Blizzard Basin ---")
 
     var solution1 = aocDay2224()
-    println("   the fewest number of minutes required to avoid the blizzards and reach the goal is $solution2")
+    println("   the fewest number of minutes required to avoid the blizzards and reach the goal is $solution1")
 
     t1 = System.currentTimeMillis() - t1
     println("puzzle solved in ${t1} ms")
